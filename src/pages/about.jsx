98e79ade6c0d55da02232b2312982d33386b6e2e
Sam Ward <sam@wordnerds.ai>
2023-06-10 11:30:25 +0100

    Update about me

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -47,35 +47,33 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’m Sam Ward. I live in New York City, where I design the
-              future.
+              I’m Sam Ward. I live in England and work fully remote.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
+                I have been a developer for over 6 years. I enjoy the creative
+                freedom from working on the front-end, whilst dealing with the
+                challenging data structures of the back-end. I started out using
+                PHP and C# at university and whilst on placement. I enjoy
+                learning about new technologies. I am currently learning Golang
+                and Python.
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                I have lived both in Australia and the UK. I spent most of my
+                time living in Western Australia, Perth which was a great
+                experience for me. I am not afraid to go outside my comfort
+                zone. As I believe that's the best way to challenge yourself.
               </p>
               <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
+                Apart from being a developer, I enjoy travelling and immersing
+                in different cultures. My aim is to travel 50 countries by the
+                time I'm 40. I care about the environment and our planet. I have
+                a keen interest renewable energy and the shift to
+                electrification.
               </p>
               <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
+                The biggest inspirations in my life are Steve Irwin and Elon
+                Musk.{' '}
               </p>
             </div>
           </div>
@@ -92,13 +90,6 @@ export default function About() {
               </SocialLink>
               <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
                 Follow on LinkedIn
-              </SocialLink>
-              <SocialLink
-                href="mailto:spencer@planetaria.tech"
-                icon={MailIcon}
-                className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
-              >
-                spencer@planetaria.tech
               </SocialLink>
             </ul>
           </div>
