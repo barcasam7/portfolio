eb635eeab5b4800253bf48bcd0f78fd74b770258
Sam Ward <sam@wordnerds.ai>
2023-06-20 20:42:07 +0100

    update social links

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -24,15 +24,15 @@ export default function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="bg-gradient-to-br from-red-600 to-yellow-300 bg-clip-text text-4xl font-bold tracking-tight text-transparent sm:text-5xl">
-            Software developer, animal lover, and traveller.
+            Software engineer, animal lover, and traveller.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I&#39;m Sam, a software Developer based in England. Welcome to my
+            I&#39;m Sam, a software engineer based in England. Welcome to my
             portfolio website where you can learn more about me and my projects
             I&#39;ve worked on.
           </p>
           <div className="mt-6 flex gap-6">
-            <SocialLink
+            {/* <SocialLink
               href="https://twitter.com"
               aria-label="Follow on Twitter"
               icon={TwitterIcon}
@@ -41,14 +41,14 @@ export default function Home() {
               href="https://instagram.com"
               aria-label="Follow on Instagram"
               icon={InstagramIcon}
-            />
+            /> */}
             <SocialLink
-              href="https://github.com"
+              href="https://github.com/barcasam7"
               aria-label="Follow on GitHub"
               icon={GitHubIcon}
             />
             <SocialLink
-              href="https://linkedin.com"
+              href="https://www.linkedin.com/in/sam-ward-b96432100/"
               aria-label="Follow on LinkedIn"
               icon={LinkedInIcon}
             />
